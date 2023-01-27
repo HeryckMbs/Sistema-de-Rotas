7ac2020704f4cccf2ca060eda983974d8058c4c7
Heryck <heryckmota@gmail.com>
2023-01-26 21:06:03 -0300

    start project

--- app/views/home.php
+++ app/views/home.php
@@ -1,4 +1,26 @@
 <?php $this->layout('master') ?>
 
+<div class="row">
+    <div class="col-md-6">
+    <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    </div>
 
-<h1>EAE COMPARSA</h1>
+
+    <div class="col-md-6">
+
+        <ul class="list-group">
+            <li class="list-group-item">
+                <a class="nav-link active" aria-current="page" href="/user">Usuário</a>
+            </li>
+            <li class="list-group-item">
+                <a class="nav-link" href="#">Apartamentos</a>
+            </li>
+            <li class="list-group-item">
+                <a class="nav-link" href="#">Cadastro de Proprietários</a>
+            </li>
+
+        </ul>
+    </div>
+</div>
--- app/views/master.php
+++ app/views/master.php
@@ -1,17 +1,69 @@
-<!DOCTYPE html>
-<html>
+
+<!doctype html>
+<html lang="en">
 
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title></title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-</head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
+</head>
 <body>
-    <?=  $this->section("content")?>
+<section>
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Service Manager Condominal</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="content-wrapper">
+        <div class="container-fluid">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-9">
+                            <h1 class="m-0">Olá</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-3 d-flex justify-content-between">
+
+                        </div>
+                    </div>
+
+                    <!-- /.row -->
+                </div>
+
+                <!-- /.container-fluid -->
+            </div>
+            <div class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <?=  $this->section("content")?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+        </div>
+    </div>
+
+</section>
+
 </body>
+
 
 </html>
